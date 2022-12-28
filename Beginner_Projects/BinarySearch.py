4de77cc53bd82fdbfe0e6a43002f7ae6bfae848f
desracto <mohammedejazzur@gmail.com>
2022-12-29 02:04:49 +0400

    Final

--- Beginner_Projects/BinarySearch.py
+++ Beginner_Projects/BinarySearch.py
@@ -1,21 +1,34 @@
-# Binary Search using a recursive method
+number_list = list(range(0, 60000000))
+num_list = list(range(0, 10))
 
-number_list = list(range(0, 10000))
-def BinarySearch(number, number_list):
-    if len(number_list) == 1:
-        if number_list[0] == number:
-            print("Found")
-            return number_list[0]
+# Binary Search using a recursive method
+def Recursive_BinarySearch(target, number_list, high, low):
+    mid = (high + low) // 2
+    if target < number_list[mid]:
+        return Recursive_BinarySearch(target, number_list, mid - 1, 0)
+    elif target > number_list[mid]:
+        return Recursive_BinarySearch(target, number_list, high, mid + 1)
+    else:
+        if target == number_list[mid]:
+            return number_list[mid]
         else:
             return -1
-    else:
-        half = len(number_list) // 2
-        if number < number_list[half]:
-            print(number_list[:half])
-            return BinarySearch(number, number_list[:half])
+
+# Binary Search using a non-recursive method
+def Iterative_BinarySearch(target, list):
+    max_index = len(list) - 1
+    min_index = 0
+
+    while max_index >= min_index:
+        half = (max_index + min_index) // 2
+        if target < list[half]:
+            max_index = half + 1
+        elif target > list[half]:
+            min_index = half + 1
         else:
-            print(number_list[half:])
-            return BinarySearch(number, number_list[half:])
+            return list[half]
+
+    return -1
 
 if __name__ == "__main__":
-    print(BinarySearch(2, number_list))
+    print(Recursive_BinarySearch(5000, number_list, len(number_list), 0))
