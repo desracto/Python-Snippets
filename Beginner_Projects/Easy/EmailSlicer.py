5afdd2824ffab42bb4b663f8997bc7da4b72dd00
desracto <theminer123321@gmail.com>
2023-07-08 04:42:34 +0400

    Reorganizing Directory

--- Beginner_Projects/Easy/EmailSlicer.py
+++
@@ -1,9 +0,0 @@
-def slice_email(email:str):
-    split = email.split("@")
-    return {"username": split[0],
-            "domain": split[1]}
-
-if __name__ == "__main__":
-    split_email = slice_email("mohammed@gmail.com")
-    print(split_email["username"])
-    print(split_email["domain"])
